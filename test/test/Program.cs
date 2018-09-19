24ee531cf7dd72fd6c9beb1ca4493c47b725eefd
Gurpreet Singh <30008846@student.toiohomai.ac.nz>
2018-09-19 16:45:01 +1200

    msg

---
+++ test/test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your weight: ");
+            double weight = double.Parse(Console.ReadLine());
+            Console.Write("Please enter your height: ");
+            double height = double.Parse(Console.ReadLine());
+            Console.Write($"Your BMI is: {BMI(weight, height)}");
+            Console.ReadLine();
+        }
+
+        static public double BMI(double weight, double height)
+        {
+            double BMI = (weight / (height * height));
+            Console.WriteLine(BMI);
+            return BMI;
+
+        }
+    }
+}
